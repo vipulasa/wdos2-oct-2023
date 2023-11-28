21632c8c9e9f485fea3006078b1e54051f7d7bb7
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2023-11-28 10:18:29 +0530

    HTML updates

--- store/hotel.js
+++ store/hotel.js
@@ -1,7 +1,9 @@
 document.addEventListener("alpine:init", () => {
   Alpine.store("hotel", {
     name: "Heritance Kandalama",
+    cover: "https://www.heritancehotels.com/kandalama/wp-content/uploads/sites/18/2022/05/Header-1.jpg",
     location: "Dambulla",
+    description: "Heritance Kandalama is an architectural masterpiece by Geoffrey Bawa, built overlooking the eighth wonder of the world - the rock fortress of Sigiriya. The hotel sits at the heart of the cultural triangle of Sri Lanka, close to five UNESCO World Heritage Sites.",
     rating: 5,
     facilities: [
         "Spa", 
